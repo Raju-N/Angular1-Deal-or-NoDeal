62247f1380c621c333d9fdff7d665229da2a73bf
Raju-N <rajuece25@gmail.com>
2017-02-01 10:44:33 +0530

    Add files via upload

---
+++ app/factory/gameService.js
@@ -0,0 +1,20 @@
+app.factory('gameService',['$http','$location',function($http,$location){
+
+
+var gameService={};
+
+gameService.boxNumber=[];
+gameService.selectedBox=0;
+gameService.playerName="";
+gameService.dealAmount=0;
+gameService.gameStarted=false;
+gameService.yourBoxAmount=0;
+
+gameService.getAmount=function()
+{
+	return $http.get("assets/data/amount.json");
+
+}
+
+return gameService;
+}])
